88a3f71906d414f444e96c54f356c6dad558ae0f
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-07-27 19:03:21 +0800

    update docker script, restart docker container

--- dev_setup.sh
+++ dev_setup.sh
@@ -12,7 +12,7 @@
 SRCDIR="$(pwd)"
 docker inspect wanchaindev > /dev/null 2>&1
 if [ $? -eq 1 ]; then
-	docker run --name wanchaindev -itd -v $SRCDIR:/wanchain/src -p 8545:8545 registry.cn-hangzhou.aliyuncs.com/wanglutech/wanchaindev /bin/sh
+	docker run --restart always --name wanchaindev -itd -v $SRCDIR:/wanchain/src -p 8545:8545 registry.cn-hangzhou.aliyuncs.com/wanglutech/wanchaindev /bin/sh
 fi
 docker exec -it wanchaindev /bin/sh
 
