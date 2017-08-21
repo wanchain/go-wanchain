798e7e762d1b4434116e7aaaaca814de02f7c0ee
liuzhihui <zhihuizhilv@foxmail.com>
2017-08-21 19:14:37 +0800

    fixed unit test bug

--- core/bench_test.go
+++ core/bench_test.go
@@ -87,7 +87,7 @@ func genValueTx(nbytes int) func(int, *BlockGen) {
 		toaddr := common.Address{}
 		data := make([]byte, nbytes)
 		gas := IntrinsicGas(data, false, false)
-		tx, _ := types.SignTx(types.NewTransaction(gen.TxNonce(benchRootAddr), toaddr, big.NewInt(1), gas, nil, data), types.HomesteadSigner{}, benchRootKey)
+		tx, _ := types.SignTx(types.NewTransaction(gen.TxNonce(benchRootAddr), toaddr, big.NewInt(1), gas, nil, data), types.HomesteadSigner{}, benchRootKey, nil)
 		gen.AddTx(tx)
 	}
 }
@@ -128,7 +128,7 @@ func genTxRing(naccounts int) func(int, *BlockGen) {
 				nil,
 				nil,
 			)
-			tx, _ = types.SignTx(tx, types.HomesteadSigner{}, ringKeys[from])
+			tx, _ = types.SignTx(tx, types.HomesteadSigner{}, ringKeys[from], nil)
 			gen.AddTx(tx)
 			from = to
 		}
