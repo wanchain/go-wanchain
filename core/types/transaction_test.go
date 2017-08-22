624b38bc75603e98620c00f55fa7563ea16c4466
liuzhihui <zhihuizhilv@foxmail.com>
2017-08-22 20:21:27 +0800

    fixed unit test bug

--- core/types/transaction_test.go
+++ core/types/transaction_test.go
@@ -52,10 +52,10 @@ var (
 )
 
 func TestTransactionSigHash(t *testing.T) {
-	if emptyTx.SigHash(HomesteadSigner{}) != common.HexToHash("c775b99e7ad12f50d819fcd602390467e28141316969f4b57f0626f74fe3b386") {
+	if emptyTx.SigHash(HomesteadSigner{}) != common.HexToHash("4085bdfcd57b2d59c6e56a6ec60b92302313f58e19b9c54eff76effaadeb85c6") {
 		t.Errorf("empty transaction hash mismatch, got %x", emptyTx.Hash())
 	}
-	if rightvrsTx.SigHash(HomesteadSigner{}) != common.HexToHash("fe7a79529ed5f7c3375d06b26b186a8644e0e16c373d7a12be41c62d6042b77a") {
+	if rightvrsTx.SigHash(HomesteadSigner{}) != common.HexToHash("fa22b6a9ead25cec443cd10bf69a807f5bcc269f943b0f319509fbdc535308ed") {
 		t.Errorf("RightVRS transaction hash mismatch, got %x", rightvrsTx.Hash())
 	}
 }
@@ -65,7 +65,7 @@ func TestTransactionEncode(t *testing.T) {
 	if err != nil {
 		t.Fatalf("encode error: %v", err)
 	}
-	should := common.FromHex("f86103018207d094b94f5374fce5edbc8e2a8697c15331677e6ebf0b0a8255441ca098ff921201554726367d2be8c804a7ff89ccf285ebc57dff8ae4c44b9c19ac4aa08887321be575c8095f789dd4c743dfe42c1820f9231f98a962b210e3ac2452a3")
+	should := common.FromHex("f8660103018207d094b94f5374fce5edbc8e2a8697c15331677e6ebf0b0a8255441ca098ff921201554726367d2be8c804a7ff89ccf285ebc57dff8ae4c44b9c19ac4aa08887321be575c8095f789dd4c743dfe42c1820f9231f98a962b210e3ac2452a3c0c0c0c0")
 	if !bytes.Equal(txb, should) {
 		t.Errorf("encoded RLP mismatch, got %x", txb)
 	}
@@ -84,43 +84,45 @@ func defaultTestKey() (*ecdsa.PrivateKey, common.Address) {
 	return key, addr
 }
 
-func TestRecipientEmpty(t *testing.T) {
-	_, addr := defaultTestKey()
-	tx, err := decodeTx(common.Hex2Bytes("f8498080808080011ca09b16de9d5bdee2cf56c28d16275a4da68cd30273e2525f3959f5d62557489921a0372ebd8fb3345f7db7b5a86d42e24d36e983e259b0664ceb8c227ec9af572f3d"))
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
+// zhihuizhilv revise:因为环签名,Sender无法从tx中获得发送者,所以该用例已过时
+//func TestRecipientEmpty(t *testing.T) {
+//	_, addr := defaultTestKey()
+//	tx, err := decodeTx(common.Hex2Bytes("f8498080808080011ca09b16de9d5bdee2cf56c28d16275a4da68cd30273e2525f3959f5d62557489921a0372ebd8fb3345f7db7b5a86d42e24d36e983e259b0664ceb8c227ec9af572f3d"))
+//	if err != nil {
+//		t.Error(err)
+//		t.FailNow()
+//	}
+//
+//	from, err := Sender(HomesteadSigner{}, tx)
+//	if err != nil {
+//		t.Error(err)
+//		t.FailNow()
+//	}
+//	if addr != from {
+//		t.Error("derived address doesn't match")
+//	}
+//}
 
-	from, err := Sender(HomesteadSigner{}, tx)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-	if addr != from {
-		t.Error("derived address doesn't match")
-	}
-}
-
-func TestRecipientNormal(t *testing.T) {
-	_, addr := defaultTestKey()
-
-	tx, err := decodeTx(common.Hex2Bytes("f85d80808094000000000000000000000000000000000000000080011ca0527c0d8f5c63f7b9f41324a7c8a563ee1190bcbf0dac8ab446291bdbf32f5c79a0552c4ef0a09a04395074dab9ed34d3fbfb843c2f2546cc30fe89ec143ca94ca6"))
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-
-	from, err := Sender(HomesteadSigner{}, tx)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-
-	if addr != from {
-		t.Error("derived address doesn't match")
-	}
-}
+// zhihuizhilv revise:因为环签名,Sender无法从tx中获得发送者,所以该用例已过时
+//func TestRecipientNormal(t *testing.T) {
+//	_, addr := defaultTestKey()
+//
+//	tx, err := decodeTx(common.Hex2Bytes("f85d80808094000000000000000000000000000000000000000080011ca0527c0d8f5c63f7b9f41324a7c8a563ee1190bcbf0dac8ab446291bdbf32f5c79a0552c4ef0a09a04395074dab9ed34d3fbfb843c2f2546cc30fe89ec143ca94ca6"))
+//	if err != nil {
+//		t.Error(err)
+//		t.FailNow()
+//	}
+//
+//	from, err := Sender(HomesteadSigner{}, tx)
+//	if err != nil {
+//		t.Error(err)
+//		t.FailNow()
+//	}
+//
+//	if addr != from {
+//		t.Error("derived address doesn't match")
+//	}
+//}
 
 // Tests that transactions can be correctly sorted according to their price in
 // decreasing order, but at the same time with increasing nonces when issued by
