8410e1ad69e86b06f02a4cdc971590de953f37c4
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-09-11 15:13:32 +0800

    change the container name

--- dev_setup.sh
+++ dev_setup.sh
@@ -10,9 +10,9 @@
 
 
 SRCDIR="$(pwd)"
-docker inspect wanchaindev > /dev/null 2>&1
+docker inspect wanchainContainer > /dev/null 2>&1
 if [ $? -eq 1 ]; then
-	docker run --restart always --name wanchaindev -itd -v $SRCDIR:/wanchain/src -p 8545:8545 registry.cn-hangzhou.aliyuncs.com/wanglutech/wanchaindev /bin/sh
+	docker run --restart always --name wanchainContainer -itd -v $SRCDIR:/wanchain/src -p 8545:8545 registry.cn-hangzhou.aliyuncs.com/wanglutech/wanchaindev /bin/sh
 fi
-docker exec -it wanchaindev /bin/sh
+docker exec -it wanchainContainer /bin/sh
 
