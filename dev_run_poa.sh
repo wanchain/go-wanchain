6713632295fd3a4de93504362d051c96826459af
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-09-12 16:36:54 +0800

    update pluto

--- dev_run_poa.sh
+++ dev_run_poa.sh
@@ -8,15 +8,15 @@
 #                                                                     
 
 
-echo "run geth in poa mode"
-mkdir -p /wanchain/data_poa
+echo "run geth in pluto testnet"
+mkdir -p /wanchain/src/data_pluto
 echo "wanglu" > /tmp/passwd.txt
-cp -rf /wanchain/data/keystore /wanchain/data_poa/
-#rm -rf /wanchain/data_poa/geth/chaindata
-#geth --datadir /wanchain/data_poa init ./genesis_example/genesis_poa.json
+cp -rf /wanchain/data/keystore /wanchain/src/data_pluto
+#rm -rf /wanchain/src/data_pluto/geth/chaindata
+#geth --datadir /wanchain/src/data_pluto init ./genesis_example/genesis_poa.json
 networkid='--pluto'
-/wanchain/src/build/bin/geth ${networkid} --nodiscover --verbosity 4 --gasprice '200000' --datadir /wanchain/data_poa --nat none \
+/wanchain/src/build/bin/geth ${networkid}  --verbosity 4 --gasprice '200000' --datadir /wanchain/src/data_pluto  \
 	--mine --minerthreads 1 \
 	 --unlock "0x2d0e7c0813a51d3bd1d08246af2a8a7a57d8922e" --password /tmp/passwd.txt \
-	 --targetgaslimit 900000000  \
+	 --targetgaslimit 900000000  --port 30303  \
 	--etherbase '0x2d0e7c0813a51d3bd1d08246af2a8a7a57d8922e'   --rpc --rpcaddr 0.0.0.0 --rpcapi "eth,personal,net,admin" --rpccorsdomain '*' 
