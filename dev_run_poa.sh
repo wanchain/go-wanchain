202393d4660e4f1c71f019eb7510f8cee6ed2adf
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-09-17 13:24:44 +0800

    update poa script

--- dev_run_poa.sh
+++ dev_run_poa.sh
@@ -9,12 +9,17 @@
 
 
 echo "run geth in pluto testnet"
-mkdir -p /wanchain/src/data_pluto
-echo "wanglu" > /tmp/passwd.txt
-cp -rf /wanchain/data/keystore /wanchain/src/data_pluto
-#rm -rf /wanchain/src/data_pluto/geth/chaindata
-#geth --datadir /wanchain/src/data_pluto init ./genesis_example/genesis_poa.json
+mkdir -p ./data_pluto
+echo "wanglu" > ./passwd.txt
+if [ -d "DOCKER" ]; then
+	cp -rf DOCKER/data/keystore ./data_pluto
+else
+	cp -rf ../data/keystore ./data_pluto
+fi
+#rm -rf ./data_pluto/geth/chaindata
+#geth --datadir ./data_pluto init ./genesis_example/genesis_poa.json
 networkid='--pluto'
-/wanchain/src/build/bin/geth ${networkid} --nat none --verbosity 4 --gasprice '200000' --datadir /wanchain/src/data_pluto  \
-	--port 30303  \
-   	--rpc --rpcaddr 0.0.0.0 --rpcapi "eth,personal,net,admin" --rpccorsdomain '*' 
+./build/bin/geth ${networkid} --nat none --verbosity 4 --gasprice '200000' --datadir ./data_pluto  \
+	 --unlock "0x2d0e7c0813a51d3bd1d08246af2a8a7a57d8922e" --password ./passwd.txt \
+	 --targetgaslimit 900000000  --port 30303  \
+   	--rpc --rpcaddr 0.0.0.0 --rpcapi "eth,personal,net,admin" --rpccorsdomain '*' $@
