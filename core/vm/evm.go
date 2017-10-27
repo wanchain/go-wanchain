bf744aeafbf7931b4323b6959bf7a94d897c9e27
JeremyJiaqg <185356122@qq.com>
2017-10-27 11:48:10 +0800

    fixed double transfer bug

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -127,7 +127,10 @@ func (evm *EVM) Call(caller ContractRef, addr common.Address, input []byte, gas 
 
 		evm.StateDB.CreateAccount(addr)
 	}
-	evm.Transfer(evm.StateDB, caller.Address(), to.Address(), value)
+	
+	if PrecompiledContracts[to.Address()] == nil {
+		evm.Transfer(evm.StateDB, caller.Address(), to.Address(), value)
+	}
 
 	// initialise a new contract and set the code that is to be used by the
 	// E The contract is a scoped evmironment for this execution context
