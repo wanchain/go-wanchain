6636b648cb889d2e228439011687b3b653d48fe7
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-11-01 23:23:58 -0700

    fix bugs

--- bootnode/dev_run_bootnode_pluto.sh
+++ bootnode/dev_run_bootnode_pluto.sh
@@ -9,16 +9,6 @@
 
 
 echo "run geth in pluto bootnode testnet"
-mkdir -p ./data_pluto/keystore
-echo "wanglu" > ./passwd.txt
-cp -rf ./UTC* ./data_pluto/keystore/
-#rm -rf /wanchain/src/data_pluto/geth/chaindata
-#geth --datadir /wanchain/src/data_pluto init ./genesis_example/genesis_poa.json
-
-networkid='--pluto'
-../build/bin/geth ${networkid}  --nodekey ./nodekey --verbosity 4 --gasprice '200000' --datadir ./data_pluto  \
-	--mine --minerthreads 1 \
-	--nat none \
-	 --unlock "0x2d0e7c0813a51d3bd1d08246af2a8a7a57d8922e" --password ./passwd.txt \
-	 --targetgaslimit 900000000  --port 30303  \
-	--etherbase '0x2d0e7c0813a51d3bd1d08246af2a8a7a57d8922e'   --rpc --rpcaddr 0.0.0.0 --rpcapi "eth,personal,net,admin" --rpccorsdomain '*' 
+echo -n ${COINBASEPW} > /tmp/pw.txt
+geth --datadir=./data --pluto --nodiscover --unlock "0xe8ffc3d0c02c0bfc39b139fa49e2c5475f000000" --password /tmp/pw.txt \
+--nodekey nodekey.key --mine --minerthreads=1
