ac604a5b853df431aa3a03fad8f3c37a4c79ad9d
JeremyJiaqg <185356122@qq.com>
2017-12-06 19:38:23 +0800

    changed value in script

--- loadScript/wancoin.js
+++ loadScript/wancoin.js
@@ -22,7 +22,7 @@ var wait = function (conditionFunc) {
 	}
 }
 
-var tranValue = 1;
+var tranValue = 10;
 
 abiDef = [{"constant":false,"type":"function","stateMutability":"nonpayable","inputs":[{"name":"OtaAddr","type":"string"},{"name":"Value","type":"uint256"}],"name":"buyCoinNote","outputs":[{"name":"OtaAddr","type":"string"},{"name":"Value","type":"uint256"}]},{"constant":false,"type":"function","inputs":[{"name":"RingSignedData","type":"string"},{"name":"Value","type":"uint256"}],"name":"refundCoin","outputs":[{"name":"RingSignedData","type":"string"},{"name":"Value","type":"uint256"}]},{"constant":false,"inputs":[],"name":"getCoins","outputs":[{"name":"Value","type":"uint256"}]}];
 
@@ -39,7 +39,7 @@ wanUnlock(eth.accounts[2]);
 var wanAddr = wan.getWanAddress(eth.accounts[2]);
 var otaAddr = wan.generateOneTimeAddress(wanAddr);
 
-txBuyData = coinContract.buyCoinNote.getData(otaAddr, web3.toWin(1));
+txBuyData = coinContract.buyCoinNote.getData(otaAddr, web3.toWin(tranValue));
 buyCoinTx = eth.sendTransaction({from:eth.accounts[1], to:coinContractAddr, value:web3.toWin(tranValue), data:txBuyData, gas: 1000000, gasprice:'0x' + (200000).toString(16)});
 wait(function(){return eth.getTransaction(buyCoinTx).blockNumber != null;});
 
@@ -55,7 +55,7 @@ privateKey = keyPairs[0];
 
 console.log("Balance of ", eth.accounts[2], " is ", web3.fromWin(eth.getBalance(eth.accounts[2])));
 var ringSignData = wan.genRingSignData(eth.accounts[2], privateKey, mixSetWith0x.join("+"))
-var txRefundData = coinContract.refundCoin.getData(ringSignData, web3.toWin(1))
+var txRefundData = coinContract.refundCoin.getData(ringSignData, web3.toWin(tranValue))
 var refundTx = eth.sendTransaction({from:eth.accounts[2], to:coinContractAddr, value:0, data:txRefundData, gas: 2000000, gasprice:'0x' + (200000).toString(16)});
 wait(function(){return eth.getTransaction(refundTx).blockNumber != null;});
 
