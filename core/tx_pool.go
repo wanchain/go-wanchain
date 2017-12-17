c6b52253b55f32a0a33ba07e73b1383863f9ebc7
zhangying888 <zymontask@gmail.com>
2017-12-18 02:31:41 +0800

    remove comment out code

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -547,11 +547,6 @@ func (pool *TxPool) local() map[common.Address]types.Transactions {
 // validateTx checks whether a transaction is valid according to the consensus
 // rules and adheres to some heuristic limits of the local node (price and size).
 func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
-
-	/*	if tx.Txtype() == 6 {
-		return nil
-	}*/
-
 	// Heuristic limit, reject transactions over 32KB to prevent DOS attacks
 	if tx.Size() > 32*1024 {
 		return ErrOversizedData
