8c8b1e0f4c6bc6dfe82550531ff3d7bcef2a2f0c
zhangying888 <zymontask@gmail.com>
2017-12-17 18:30:03 +0800

    change testcase code

--- contracts/release/contract_test.go
+++ contracts/release/contract_test.go
@@ -32,15 +32,14 @@ import (
 // contract for testing.
 func setupReleaseTest(t *testing.T, prefund ...*ecdsa.PrivateKey) (*ecdsa.PrivateKey, *ReleaseOracle, *backends.SimulatedBackend) {
 	// Generate a new random account and a funded simulator
-	// key, _ := crypto.GenerateKey()
-	key, _ := crypto.HexToECDSA("3efdddbf163faf1b5ec73e833b7820e87560137917773f63b7dc33e1dcb6dd24")
+	key, _ := crypto.GenerateKey()
 	auth := bind.NewKeyedTransactor(key)
 
 	alloc := core.GenesisAlloc{auth.From: {Balance: big.NewInt(10000000000)}}
 	for _, key := range prefund {
 		alloc[crypto.PubkeyToAddress(key.PublicKey)] = core.GenesisAccount{Balance: big.NewInt(10000000000)}
 	}
-	sim := backends.NewSimulatedBackend(alloc)
+	sim := backends.NewSimulatedBackendEx(alloc)
 
 	// Deploy a version oracle contract, commit and return
 	_, _, oracle, err := DeployReleaseOracle(auth, sim, []common.Address{auth.From})
