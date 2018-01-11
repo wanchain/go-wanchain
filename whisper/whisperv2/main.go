f42993d64855469e90e19d1bd1e1addb06fc492e
Michael <michael@wanchain.org>
2018-01-10 19:43:07 -0500

    convert Ethereum -> Wanchain in output strings

--- whisper/whisperv2/main.go
+++ whisper/whisperv2/main.go
@@ -56,9 +56,9 @@ func main() {
 		ListenAddr: ":30300",
 		NAT:        nat.Any(),
 	}
-	fmt.Println("Starting Ethereum peer...")
+	fmt.Println("Starting Wanchain peer...")
 	if err := server.Start(); err != nil {
-		fmt.Printf("Failed to start Ethereum peer: %v.\n", err)
+		fmt.Printf("Failed to start Wanchain peer: %v.\n", err)
 		os.Exit(1)
 	}
 
