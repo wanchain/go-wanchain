9b1f658e4ea303df583a7744c5127c39b974bae2
lansehuiyi6 <zhan.li@wanglutech.com>
2018-01-16 09:58:17 +0000

    change geth to gwan

--- networkScript/wanchainBackup.sh
+++ networkScript/wanchainBackup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# back up go-wanchain geth data
+# back up go-wanchain gwan data
 # hot backup will use admin.exportChain to backup
-# cold backup will kill geth first, tar, then restart
+# cold backup will kill gwan first, tar, then restart
 
 
 #   __        ___    _   _  ____ _           _       ____             
@@ -16,8 +16,9 @@
 
 # 
 
-version="v0.9.4"
+version=""
 WANPATH=$HOME/wanchain/$version
+wanchainLogDir=$HOME/wanchainbackup/logbackup
 wanchainLogPath=$HOME/wanchainbackup/logbackup/wanchainlog.txt
 HOTBACKUPDIR=$HOME/wanchainbackup/hotbackup
 COLDBACKUPDIR=$HOME/wanchainbackup/coldbackup
@@ -25,7 +26,7 @@ restartflag=0
 backupNum=56
 
 ipcPath=$HOME/.wanchain/gwan.ipc
-GETHDIR=$HOME/.wanchain/geth
+GWANDIR=$HOME/.wanchain/gwan
 
 hotlog=$HOTBACKUPDIR/log.txt
 coldlog=$COLDBACKUPDIR/log.txt
@@ -41,12 +42,12 @@ echo "****** go-wanchain data hot Backup begin ******" >> $hotlog
 echo "************************************************" >> $hotlog
 
 DATE=`date '+%Y%m%d-%H%M%S'`
-backupChainName=$DATE"-wanchain"
+backupChainName=$DATE"-wanchain.tar.gz"
 
 echo " *** BACKUPTIME: " $DATE >> $hotlog
 
 echo " " >> $hotlog
-echo "admin.exportChain('$HOTBACKUPDIR/$backupChainName')" | ./bin/geth attach ipc:$ipcPath exit 2>&1 >> $hotlog
+echo "admin.exportChain('$HOTBACKUPDIR/$backupChainName')" | ./bin/gwan attach ipc:$ipcPath exit >> $hotlog 2>&1 
 echo " *** BACKUP Chain Name: " $backupChainName >> $hotlog
 
 if [ $(ls $HOTBACKUPDIR -l | grep "wanchain" | wc -l) -gt $backupNum ]
@@ -73,50 +74,64 @@ echo "****** go-wanchaia data cold Backup begin ******" >> $coldlog
 echo "************************************************" >> $coldlog
 
 DATE=`date '+%Y%m%d-%H%M%S'`
-backupGethName=$DATE"-geth.tar"
+backupGwanName=$DATE"-gwan.tar.gz"
 echo " *** BACKUPTIME:" $DATE >> $coldlog
 	
-cd $GETHDIR
-echo " " >> $coldlog
-
-if [ $(ps -ef | grep geth | grep -v 'grep\|attach\|daemon_geth' | wc -l) -gt 0 ]
-then
-    echo "before compress wan-chain geth data, the geth process will be killed" >> $coldlog
-    proinfo=`ps -ef | grep geth | grep -v 'grep\|attach\|daemon_geth'`
-    label=`echo $proinfo | awk '{print $7}'`
-    CMDinfo=`echo ${proinfo#*$label}`
-    echo "follow process will be killed" >> $coldlog
-    echo $CMDinfo >> $coldlog
-    ps -ef | grep geth | grep -v 'grep\|attach\|daemon_geth' | awk '{print $2}' | xargs kill -9
-    restartflag=1
-fi
-
-echo "begin to tar geth file under " $GETHDIR >> $coldlog
-echo " *** filelist *** " >> $coldlog
-tar czvf $COLDBACKUPDIR/$backupGethName * >> $coldlog 2>&1
-
+cd $GWANDIR >> $coldlog 2>&1
 if [ $? -eq 0 ]; then
-    echo " *** BACKUP Geth data Name:" $backupGethName " Successsful">> $coldlog
+    echo " " >> $coldlog
+
+    if [ $(ps -ef | grep gwan | grep -v 'grep\|attach\|daemon_gwan\|defunct' | wc -l) -gt 0 ]
+    then
+	echo "before compress wan-chain gwan data, the gwan process will be killed" >> $coldlog
+	proinfo=`ps -ef | grep gwan | grep -v 'grep\|attach\|daemon_gwan\|defunct'`
+	label=`echo $proinfo | awk '{print $7}'`
+	CMDinfo=`echo ${proinfo#*$label}`
+	echo "follow process will be killed" >> $coldlog
+	echo $CMDinfo >> $coldlog
+	ps -ef | grep gwan | grep -v 'grep\|attach\|daemon_gwan\|defunct' | awk '{print $2}' | xargs kill -9
+	restartflag=1
+    fi
+
+    echo "begin to tar gwan file under " $GWANDIR >> $coldlog
+    echo " *** filelist *** " >> $coldlog
+    tar czvf $COLDBACKUPDIR/$backupGwanName * >> $coldlog 2>&1
+
+    if [ $? -eq 0 ]; then
+	echo " *** BACKUP Gwan data Name:" $backupGwanName " Successsful">> $coldlog
+    else
+	echo " *** BACKUP Gwan data Name:" $backupGwanName " Fail!">> $coldlog
+    fi
+
+    if [ $(ls $COLDBACKUPDIR -l | grep "gwan.tar" | wc -l) -gt $backupNum ]
+    then
+	echo "the backup number in folder" $COLDBACKUPDIR " is larger than " $backupNum ", follow files would be rm" >> $coldlog
+	ls $COLDBACKUPDIR -rt | head -n1 >> $coldlog
+	cd $COLDBACKUPDIR
+	rm -r $(ls $COLDBACKUPDIR -rt | head -n1) >> $coldlog
+    fi
+
+    echo "****** go-wanchain cold Backup end******" >> $coldlog
+    echo " " >> $coldlog
+    echo " " >> $coldlog
+    echo " " >> $coldlog
+
+    if [ $restartflag -eq 1 ]
+    then
+	#This will recall the gwan command
+	echo "re call the follow process" >> $coldlog
+	echo $CMDinfo >> $coldlog
+	cd $WANPATH  
+	if [ ! -d $wanchainLogDir ] ; then
+            mkdir -p "$wanchainLogDir"
+            echo "wanchainLogDir not exist: "$wanchainLogDir", create it!"
+        fi
+	$CMDinfo >> $wanchainLogPath 2>&1
+    fi
 else
-    echo " *** BACKUP Geth data Name:" $backupGethName " Fail!">> $coldlog
-fi
-
-if [ $(ls $COLDBACKUPDIR -l | grep "geth.tar" | wc -l) -gt $backupNum ]
-then
-    echo "the backup number in folder" $COLDBACKUPDIR " is larger than " $backupNum ", follow files would be rm" >> $coldlog
-    ls $COLDBACKUPDIR -rt | head -n1 >> $coldlog
-    cd $COLDBACKUPDIR
-    rm -r $(ls $COLDBACKUPDIR -rt | head -n1) >> $coldlog
-fi
-
-echo "****** go-wanchain cold Backup end******" >> $coldlog
-echo " " >> $coldlog
-echo " " >> $coldlog
-echo " " >> $coldlog
-
-if [ $restartflag -eq 1 ]
-then
-    #This will recall the geth command
-    cd $WANPATH  
-    $CMDinfo >> $wanchainLogPath 2>&1
+    echo " *** BACKUP GWANDIR dir not exist:" $GWANDIR " BACKUP Fail!">> $coldlog
+    echo "****** go-wanchain cold Backup end******" >> $coldlog
+    echo " " >> $coldlog
+    echo " " >> $coldlog
+    echo " " >> $coldlog
 fi
