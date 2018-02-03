b3f6e7a049e4410efff912f9f16e48598107d687
coranos <coranos@users.noreply.github.com>
2018-02-03 16:11:38 -0600

    update hd.go's ledger derivation path
    update hd.go's ledger derivation path to be consistent with bip44.
    
    wanchain's coin ID is 5718350 according to the document of record.
    
    https://github.com/satoshilabs/slips/blob/master/slip-0044.md
    
    this change will make go-wanchain consistent with blue-app-wanchain.
    
    https://github.com/coranos/blue-app-wanchain/blob/master/Makefile#L43

--- accounts/hd.go
+++ accounts/hd.go
@@ -37,7 +37,7 @@ var DefaultBaseDerivationPath = DerivationPath{0x80000000 + 44, 0x80000000 + 60,
 // DefaultLedgerBaseDerivationPath is the base path from which custom derivation endpoints
 // are incremented. As such, the first account will be at m/44'/60'/0'/0, the second
 // at m/44'/60'/0'/1, etc.
-var DefaultLedgerBaseDerivationPath = DerivationPath{0x80000000 + 44, 0x80000000 + 60, 0x80000000 + 0, 0}
+var DefaultLedgerBaseDerivationPath = DerivationPath{0x80000000 + 44, 0x80000000 + 5718350, 0x80000000 + 0, 0}
 
 // DerivationPath represents the computer friendly version of a hierarchical
 // deterministic wallet account derivaion path.
