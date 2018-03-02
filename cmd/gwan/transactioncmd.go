fbf09d554b7b49f4dd8770f28e89f975744f28bd
zhihuizhilv <zhihuizhilv@163.com>
2018-03-02 17:22:19 +0800

    add gwan transaction exporthex/exportraw command

--- cmd/gwan/transactioncmd.go
+++ cmd/gwan/transactioncmd.go
@@ -18,11 +18,13 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"github.com/wanchain/go-wanchain/cmd/utils"
 	"github.com/wanchain/go-wanchain/common"
 	"github.com/wanchain/go-wanchain/core"
 	"github.com/wanchain/go-wanchain/core/types"
+	"github.com/wanchain/go-wanchain/rlp"
 	"gopkg.in/urfave/cli.v1"
 	"os"
 )
@@ -48,6 +50,30 @@ will export the transaction using setting as hash from datadir chain, and save a
 	geth --datadir ./data transaction export 0x1111111111111111111111111111111111111111111111111111111111111111 ./tx.json
 
 will export the transaction using setting as hash from datadir chain, and save as ./tx.json .`,
+			},
+
+			{
+				Name:     "exportraw",
+				Usage:    "exportraw transaction save as json file",
+				Action:   utils.MigrateFlags(exportTransactionRaw),
+				Category: "TRANSACTION COMMANDS",
+				Flags:    []cli.Flag{},
+				Description: `
+	geth --datadir ./data transaction export 0x1111111111111111111111111111111111111111111111111111111111111111 ./tx.dat
+
+will export the transaction using setting as hash from datadir chain, and save as ./tx.raw .`,
+			},
+
+			{
+				Name:     "exporthex",
+				Usage:    "exporthex transaction save as json file",
+				Action:   utils.MigrateFlags(exportTransactionHex),
+				Category: "TRANSACTION COMMANDS",
+				Flags:    []cli.Flag{},
+				Description: `
+	geth --datadir ./data transaction export 0x1111111111111111111111111111111111111111111111111111111111111111 ./tx.txt
+
+will export the transaction using setting as hash from datadir chain, and save as ./tx.txt .`,
 			},
 		},
 	}
@@ -84,5 +110,73 @@ func exportTransaction(ctx *cli.Context) error {
 	defer fh.Close()
 
 	_, err = fh.Write(out)
+	return err
+}
+
+func exportTransactionRaw(ctx *cli.Context) error {
+	args := ctx.Args()
+	if len(args) < 2 {
+		return errors.New("args count not enough")
+	}
+
+	hashStr := args[0]
+	filePath := args[1]
+
+	hash := common.HexToHash(hashStr)
+	stack := makeFullNode(ctx)
+	_, chainDb := utils.MakeChain(ctx, stack)
+
+	// Try to return an already finalized transaction
+	var tx *types.Transaction
+	if tx, _, _, _ = core.GetTransaction(chainDb, hash); tx == nil {
+		return errors.New("no found tx")
+	}
+
+	buf := new(bytes.Buffer)
+	if err := rlp.Encode(buf, tx); err != nil {
+		return err
+	}
+
+	fh, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+
+	_, err = fh.Write(buf.Bytes())
+	return err
+}
+
+func exportTransactionHex(ctx *cli.Context) error {
+	args := ctx.Args()
+	if len(args) < 2 {
+		return errors.New("args count not enough")
+	}
+
+	hashStr := args[0]
+	filePath := args[1]
+
+	hash := common.HexToHash(hashStr)
+	stack := makeFullNode(ctx)
+	_, chainDb := utils.MakeChain(ctx, stack)
+
+	// Try to return an already finalized transaction
+	var tx *types.Transaction
+	if tx, _, _, _ = core.GetTransaction(chainDb, hash); tx == nil {
+		return errors.New("no found tx")
+	}
+
+	buf := new(bytes.Buffer)
+	if err := rlp.Encode(buf, tx); err != nil {
+		return err
+	}
+
+	fh, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+
+	_, err = fh.Write([]byte(common.ToHex(buf.Bytes())))
 	return err
 }
