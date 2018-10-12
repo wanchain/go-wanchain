4f45c3a193f6f2365dda17c8e2f401ba72be2e06
zhihuizhilv <zhihuizhilv@foxmail.com>
2018-10-12 13:27:16 +0800

    v1.1.3_wan-mpc

--- params/version.go
+++ params/version.go
@@ -22,8 +22,8 @@ import (
 
 const (
 	VersionMajor = 1         // Major version component of the current release
-	VersionMinor = 0         // Minor version component of the current release
-	VersionPatch = 1      	 // Patch version component of the current release
+	VersionMinor = 1         // Minor version component of the current release
+	VersionPatch = 3      	 // Patch version component of the current release
 	VersionMeta  = "wan_mpc" // Version metadata to append to the version string
 )
 
--- storeman/storemanmpc/step/mpc_jrss_generator.go
+++ storeman/storemanmpc/step/mpc_jrss_generator.go
@@ -6,8 +6,8 @@ import (
 	mpcprotocol "github.com/wanchain/go-wanchain/storeman/storemanmpc/protocol"
 	mpcsyslog "github.com/wanchain/go-wanchain/storeman/syslog"
 	"math/big"
-	"github.com/wanchain/go-wanchain/log"
-	"github.com/wanchain/go-wanchain/common"
+	//"github.com/wanchain/go-wanchain/log"
+	//"github.com/wanchain/go-wanchain/common"
 )
 
 type RandomPolynomialValue struct {
@@ -38,7 +38,7 @@ func (poly *RandomPolynomialValue) initialize(peers *[]mpcprotocol.PeerInfo, res
 		poly.randCoefficient[0] = *big.NewInt(0)
 	}
 
-	log.Warn("-----------------RandomPolynomialValue.initialize", "randCoefficient 0", common.ToHex(poly.randCoefficient[0].Bytes()))
+	// log.Warn("-----------------RandomPolynomialValue.initialize", "randCoefficient 0", common.ToHex(poly.randCoefficient[0].Bytes()))
 
 	for i := 0; i < len(poly.polyValue); i++ {
 		poly.polyValue[i] = mpccrypto.EvaluatePoly(poly.randCoefficient, new(big.Int).SetUint64((*peers)[i].Seed))
