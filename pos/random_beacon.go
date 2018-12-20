6fc920d11f4ea8ebd87c537f9b85c5205530e161
zhihuizhilv <zhihuizhilv@foxmail.com>
2018-12-20 09:58:05 +0800

    add random beacon code

---
+++ pos/random_beacon.go
@@ -0,0 +1,12 @@
+package pos
+
+func Loop() error {
+	// get epoch id, slot id
+
+	// check 4K 8K point
+
+	// do 4K
+
+	// do 8K
+
+}
---
+++ pos/tx_creater.go
@@ -0,0 +1,15 @@
+package pos
+
+import "github.com/wanchain/go-wanchain/core/types"
+
+func CreateRBDKGTx() *types.Transaction {
+	// ************
+	return nil
+}
+
+func CreateRBSIGTx() *types.Transaction {
+	// *************
+	return nil
+}
+
+
