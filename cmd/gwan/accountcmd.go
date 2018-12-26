c664ee17bdf3c1bd49815cd1c69b98c071709538
mutouji <59022665@qq.com>
2018-12-25 22:26:43 -0800

    add method to show public keys of an address

--- cmd/gwan/accountcmd.go
+++ cmd/gwan/accountcmd.go
@@ -17,7 +17,10 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
+	"github.com/wanchain/go-wanchain/common"
+	"github.com/wanchain/go-wanchain/crypto"
 	"io/ioutil"
 
 	"github.com/wanchain/go-wanchain/accounts"
@@ -186,6 +189,17 @@ As you can directly copy your encrypted accounts to another ethereum instance,
 this import mechanism is not needed when you transfer an account between
 nodes.
 `,
+			},
+			{
+				Name:   "pubkeys",
+				Usage:  "Print public keys",
+				Action: utils.MigrateFlags(showPublicKey),
+				Flags: []cli.Flag{
+					utils.DataDirFlag,
+					utils.KeyStoreDirFlag,
+				},
+				Description: `
+Print public key of an address`,
 			},
 		},
 	}
@@ -361,5 +375,41 @@ func accountImport(ctx *cli.Context) error {
 		utils.Fatalf("Could not create the account: %v", err)
 	}
 	fmt.Printf("Address: {%s}\n", acct.Address.Hex()[2:])
+	return nil
+}
+
+func showPublicKey(ctx *cli.Context) error {
+	addrstr := ctx.Args().Get(0)
+	passwd := ctx.Args().Get(1)
+	if len(addrstr) == 0 {
+		utils.Fatalf("address must be given as argument")
+	}
+	if len(passwd) == 0 {
+		utils.Fatalf("passwd must be given as argument")
+	}
+
+	stack, _ := makeConfigNode(ctx)
+	ks := stack.AccountManager().Backends(keystore.KeyStoreType)[0].(*keystore.KeyStore)
+	addr := common.HexToAddress(addrstr)
+	all := ks.Accounts()
+	lenth := len(all)
+	for i := 0; i < lenth; i++ {
+		if all[i].Address == addr {
+			key, err := ks.GetKey(all[i], passwd)
+			if err != nil {
+				utils.Fatalf("Error failed to load keyfile ")
+			}
+			if key.PrivateKey != nil {
+				fmt.Println("key1:" + hex.EncodeToString(crypto.FromECDSAPub(&key.PrivateKey.PublicKey)))
+			}
+			if key.PrivateKey2 != nil {
+				fmt.Println("key2:" + hex.EncodeToString(crypto.FromECDSAPub(&key.PrivateKey2.PublicKey)))
+			}
+			if key.PrivateKey3 != nil {
+				fmt.Println("key3:" + hex.EncodeToString(key.PrivateKey3.G1.Marshal()))
+			}
+			break
+		}
+	}
 	return nil
 }
