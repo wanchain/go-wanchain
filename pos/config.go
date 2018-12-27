8e93b1118ff5a73f159a7a6c8396ce6aaf97600f
mutouji <59022665@qq.com>
2018-12-27 00:33:06 -0800

    test for part commit

--- pos/config.go
+++ pos/config.go
@@ -15,7 +15,7 @@ type Config struct {
 	SelfPrK *big.Int
 }
 
-var DefaultConfig Config = Config{1,2,1, 0, 0, new(bn256.G1).ScalarBaseMult(big.NewInt(1)), big.NewInt(1)}
+var DefaultConfig Config = Config{1,1,1, 0, 0, new(bn256.G1).ScalarBaseMult(big.NewInt(1)), big.NewInt(1)}
 
 func Cfg() *Config {
 	return &DefaultConfig;
