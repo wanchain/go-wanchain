9768b7617edef1c3566cf0b591bdcd3a5d09258d
MoLin <molin@wanchain.org>
2019-01-02 11:22:10 +0800

    update gas

--- pos/slotleader/slot_leader_selection.go
+++ pos/slotleader/slot_leader_selection.go
@@ -989,6 +989,7 @@ func (s *SlotLeaderSelection) sendStage1Tx(data []byte) error {
 	arg["from"] = s.key.Address
 	arg["to"] = vm.GetSlotLeaderSCAddress()
 	arg["value"] = (*hexutil.Big)(big.NewInt(0))
+	arg["gas"] = (*hexutil.Big)(big.NewInt(100000))
 	arg["txType"] = 1
 	arg["data"] = hexutil.Bytes(payload)
 	log.Debug("Write data of payload", "length", len(payload))
