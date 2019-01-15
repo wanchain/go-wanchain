45c50785ba4f1755916cf13d7342798533e60614
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2019-01-15 16:21:32 +0800

    check generateSMA logic and update code

--- core/vm/slot_leader_select_contracts.go
+++ core/vm/slot_leader_select_contracts.go
@@ -304,11 +304,13 @@ func isInValidStage(epochID uint64, evm *EVM, kStart uint64, kEnd uint64) bool {
 	eid, sid := postools.CalEpochSlotID(evm.Time.Uint64())
 	if epochID != eid {
 		log.Warn("Tx epochID is not current epoch", "epochID", eid, "slotID", sid, "currentEpochID", epochID)
+
 		return false
 	}
 
 	if sid > kEnd || sid < kStart {
 		log.Warn("Tx is out of valid stage range", "epochID", eid, "slotID", sid, "rangeStart", kStart, "rangeEnd", kEnd)
+
 		return false
 	}
 
