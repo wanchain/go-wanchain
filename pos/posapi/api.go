c72bcb3c6cdec20e93598890c6e450a01b3bbc5b
codeman <mutouji1@163.com>
2019-01-21 00:36:05 -0800

    add log for debug

--- pos/posapi/api.go
+++ pos/posapi/api.go
@@ -139,3 +139,21 @@ func (a PosApi) Random(epochId uint64, blockNr int64) (*big.Int, error) {
 
 	return r, nil
 }
+
+func (a PosApi) GetSijCount(epochId uint64, blockNr int64) (int, error) {
+	state, _, err := a.backend.StateAndHeaderByNumber(context.Background(), rpc.BlockNumber(blockNr))
+	if err != nil {
+		return 0, err
+	}
+	j := 0
+	for i:=0; i<pos.RandomProperCount; i++ {
+		sigData, err := vm.GetSig(state, epochId, uint32(i))
+		if err != nil {
+			return 0, err
+		}
+		if sigData != nil {
+			j++
+		}
+	}
+	return j, nil
+}
