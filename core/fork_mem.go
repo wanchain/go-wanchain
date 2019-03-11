8b42f754ffa6a2bd31190ebe3530e9b238c2c42c
MoLin <molin@wanchain.org>
2019-03-11 15:31:54 +0800

    update

--- core/fork_mem.go
+++ core/fork_mem.go
@@ -1,51 +1,45 @@
 package core
 
 import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"math/big"
+	"sort"
+	"sync"
+
 	"github.com/wanchain/go-wanchain/common"
 	"github.com/wanchain/go-wanchain/core/types"
 	"github.com/wanchain/go-wanchain/pos"
-	"fmt"
-	"errors"
-	"sync"
-	"math/big"
-		"sort"
-	"encoding/binary"
 	"github.com/wanchain/go-wanchain/pos/posdb"
-	"github.com/wanchain/go-wanchain/pos/slotleader"
-	"github.com/wanchain/go-wanchain/pos/epochLeader"
-	"github.com/wanchain/go-wanchain/crypto"
-	"encoding/json"
 )
 
 type chainType uint
-const(
-	BLOCKCHAIN = iota //0
-	HEADERCHAIN //1
+
+const (
+	BLOCKCHAIN  = iota //0
+	HEADERCHAIN        //1
 )
 
-
 type EpochGenesis struct {
-	ProtocolMagic			[]byte	 	//magic number
-	EpochId				  	uint64	  	//current epochId
-	PreEpochLastBlkHash  	common.Hash 	//the hash of last block of previous epoch
-	SlotLeaders			  	[][]byte 	//current epoch slotleaders
-	RBLeaders			  	[][]byte 	//current epoch slotleaders
-	GenesisBlkHash		  	common.Hash	//the hash of this block
-	Extra				  	[]byte   	//empty
+	ProtocolMagic       []byte      //magic number
+	EpochId             uint64      //current epochId
+	PreEpochLastBlkHash common.Hash //the hash of last block of previous epoch
+	SlotLeaders         [][]byte    //current epoch slotleaders
+	RBLeaders           [][]byte    //current epoch slotleaders
+	GenesisBlkHash      common.Hash //the hash of this block
+	Extra               []byte      //empty
 }
 
 type ForkMemBlockChain struct {
-	ctype 			chainType
-	kBufferedChains  map[string][]common.Hash
-	kBufferedBlks	 map[common.Hash]*types.Block
-	curMaxBlkNum	 int64
-	lock sync.RWMutex
+	ctype           chainType
+	kBufferedChains map[string][]common.Hash
+	kBufferedBlks   map[common.Hash]*types.Block
+	curMaxBlkNum    int64
+	lock            sync.RWMutex
 }
 
-
-
-
-func NewForkMemBlockChain(ctype chainType) *ForkMemBlockChain{
+func NewForkMemBlockChain(ctype chainType) *ForkMemBlockChain {
 
 	f := &ForkMemBlockChain{}
 	f.ctype = ctype
@@ -55,7 +49,7 @@ func NewForkMemBlockChain(ctype chainType) *ForkMemBlockChain{
 	return f
 }
 
-type BlockSorter [] *types.Block
+type BlockSorter []*types.Block
 
 //Len()
 func (s BlockSorter) Len() int {
@@ -70,7 +64,6 @@ func (s BlockSorter) Less(i, j int) bool {
 func (s BlockSorter) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
-
 
 func (f *ForkMemBlockChain) calEpochSlotIDFromTime(timeUnix uint64) (epochId uint64, slotId uint64) {
 	if pos.EpochBaseTime == 0 {
@@ -102,8 +95,7 @@ func (f *ForkMemBlockChain) GetBlockEpochIdAndSlotId(header *types.Header) (blkE
 	return
 }
 
-
-func (f *ForkMemBlockChain) Maxvalid(workBlk *types.Block) (types.Blocks,error){
+func (f *ForkMemBlockChain) Maxvalid(workBlk *types.Block) (types.Blocks, error) {
 
 	f.lock.Lock()
 	defer f.lock.Unlock()
@@ -112,16 +104,16 @@ func (f *ForkMemBlockChain) Maxvalid(workBlk *types.Block) (types.Blocks,error){
 	var midSidBlk *types.Block
 
 	if workBlk == nil {
-		return nil,errors.New("can not get current block in working chain")
+		return nil, errors.New("can not get current block in working chain")
 	}
 
 	workBlkNum := int64(workBlk.NumberU64())
 
-	fmt.Println("begin select maxvalid workblk=",workBlkNum)
+	fmt.Println("begin select maxvalid workblk=", workBlkNum)
 
 	//if work block is in the highest one or higher than buffer,use work blk,work chain will not change
 	if workBlkNum >= f.curMaxBlkNum {
-		return nil,nil
+		return nil, nil
 	}
 
 	maxNumKey := big.NewInt(int64(f.curMaxBlkNum)).Text(10)
@@ -142,7 +134,7 @@ func (f *ForkMemBlockChain) Maxvalid(workBlk *types.Block) (types.Blocks,error){
 				continue
 			}
 
-			fmt.Print("maxvalid block hash",common.ToHex(hs[:]),"  block number",blk.NumberU64(),epidNew,sid)
+			fmt.Print("maxvalid block hash", common.ToHex(hs[:]), "  block number", blk.NumberU64(), epidNew, sid)
 
 			if epidOld == 0 {
 				epidOld = epidNew
@@ -154,50 +146,49 @@ func (f *ForkMemBlockChain) Maxvalid(workBlk *types.Block) (types.Blocks,error){
 			}
 		}
 	}
-		// reduce new chain
+	// reduce new chain
 	for ; midSidBlk != nil && int64(midSidBlk.NumberU64()) != workBlkNum; midSidBlk = f.kBufferedBlks[midSidBlk.ParentHash()] {
 		chainBlks = append(chainBlks, midSidBlk)
 	}
 
-		//find common prefix
+	//find common prefix
 	if midSidBlk != nil && midSidBlk.Hash() != workBlk.Hash() {
 		for {
-				chainBlks = append(chainBlks, midSidBlk)
-				if (workBlk!=nil && workBlk.NumberU64()==1)||(workBlk!=nil&&workBlk.Hash()==midSidBlk.Hash()&&workBlk.NumberU64()==midSidBlk.NumberU64()) {
-					break
-				}
+			chainBlks = append(chainBlks, midSidBlk)
+			if (workBlk != nil && workBlk.NumberU64() == 1) || (workBlk != nil && workBlk.Hash() == midSidBlk.Hash() && workBlk.NumberU64() == midSidBlk.NumberU64()) {
+				break
+			}
 
-				midSidBlk = f.kBufferedBlks[midSidBlk.ParentHash()]
-				if midSidBlk == nil {
-					return nil, errors.New("can not find common prefix")
-				}
+			midSidBlk = f.kBufferedBlks[midSidBlk.ParentHash()]
+			if midSidBlk == nil {
+				return nil, errors.New("can not find common prefix")
+			}
 
-				workBlk = f.kBufferedBlks[workBlk.ParentHash()]
-			 	if workBlk == nil {
-					return nil, errors.New("can not find common prefix")
-				}
+			workBlk = f.kBufferedBlks[workBlk.ParentHash()]
+			if workBlk == nil {
+				return nil, errors.New("can not find common prefix")
+			}
 
 		}
 	}
 
 	sort.Sort(BlockSorter(chainBlks))
 	fmt.Println("maxValid \n\n")
-	for _,blk := range chainBlks {
-		fmt.Println("blkNum=",blk.NumberU64()," hash=",common.ToHex(blk.Hash().Bytes()))
+	for _, blk := range chainBlks {
+		fmt.Println("blkNum=", blk.NumberU64(), " hash=", common.ToHex(blk.Hash().Bytes()))
 	}
 
-	fmt.Println("end select maxvalid workBlkNum=",workBlkNum)
+	fmt.Println("end select maxvalid workBlkNum=", workBlkNum)
 	return chainBlks, nil
 }
 
-
-func (f *ForkMemBlockChain) PushHeaders(headerChain []*types.Header) error{
+func (f *ForkMemBlockChain) PushHeaders(headerChain []*types.Header) error {
 	fmt.Println("begin pushHeaders")
 	if f.ctype != HEADERCHAIN {
 		return errors.New("error chain type which require HEADERCHAIN")
 	}
 
-	for _,header := range headerChain {
+	for _, header := range headerChain {
 
 		blk := types.NewBlockWithHeader(header)
 		err := f.Push(blk)
@@ -210,14 +201,14 @@ func (f *ForkMemBlockChain) PushHeaders(headerChain []*types.Header) error{
 	return nil
 }
 
-func (f *ForkMemBlockChain) PushBlocks(blockChain types.Blocks) error{
+func (f *ForkMemBlockChain) PushBlocks(blockChain types.Blocks) error {
 	fmt.Println("push block begin")
 
 	if f.ctype != BLOCKCHAIN {
 		return errors.New("error chain type which require BLOCKCHAIN")
 	}
 
-	for _,blk := range blockChain {
+	for _, blk := range blockChain {
 		err := f.Push(blk)
 		if err != nil {
 			return err
@@ -228,8 +219,7 @@ func (f *ForkMemBlockChain) PushBlocks(blockChain types.Blocks) error{
 	return nil
 }
 
-
-func (f *ForkMemBlockChain) Push(block *types.Block) error{
+func (f *ForkMemBlockChain) Push(block *types.Block) error {
 	f.lock.Lock()
 	defer f.lock.Unlock()
 
@@ -239,14 +229,14 @@ func (f *ForkMemBlockChain) Push(block *types.Block) error{
 		f.curMaxBlkNum = newbn
 	} else {
 		//input need to be continous block
-		if f.curMaxBlkNum + 1 == newbn {
+		if f.curMaxBlkNum+1 == newbn {
 			f.curMaxBlkNum = newbn
 		}
 	}
 
 	num := block.Number().Text(10)
 
-	f.kBufferedChains[num] = append(f.kBufferedChains[num],block.Hash())
+	f.kBufferedChains[num] = append(f.kBufferedChains[num], block.Hash())
 	f.kBufferedBlks[block.Hash()] = block
 
 	return nil
@@ -269,40 +259,35 @@ func (f *ForkMemBlockChain) PopBack() {
 
 		blkHashs := f.kBufferedChains[bnText.Text(10)]
 
-		for _,bh := range blkHashs {
-			delete(f.kBufferedBlks,bh)
+		for _, bh := range blkHashs {
+			delete(f.kBufferedBlks, bh)
 		}
 
-		delete(f.kBufferedChains,bnText.Text(10))
+		delete(f.kBufferedChains, bnText.Text(10))
 	}
 
 	return
 }
 
-
 func (f *ForkMemBlockChain) PrintAllBffer() {
 
-	for idx,blkHashs := range f.kBufferedChains {
-		for _,bh := range blkHashs {
-			fmt.Println("block number=",idx," hash=",common.ToHex(bh[:]))
+	for idx, blkHashs := range f.kBufferedChains {
+		for _, bh := range blkHashs {
+			fmt.Println("block number=", idx, " hash=", common.ToHex(bh[:]))
 		}
 	}
 
-	for _,blk := range f.kBufferedBlks {
-		epid, sid,_:= f.GetBlockEpochIdAndSlotId(blk.Header())
-		fmt.Println(" hash=",common.ToHex(blk.Hash().Bytes())," epid=",epid," sid=",sid)
+	for _, blk := range f.kBufferedBlks {
+		epid, sid, _ := f.GetBlockEpochIdAndSlotId(blk.Header())
+		fmt.Println(" hash=", common.ToHex(blk.Hash().Bytes()), " epid=", epid, " sid=", sid)
 	}
 }
 
-
-
-
-func (f *ForkMemBlockChain) updateReOrg(epochId uint64,length uint64) {
+func (f *ForkMemBlockChain) updateReOrg(epochId uint64, length uint64) {
 	reOrgDb := posdb.GetDbByName("forkdb")
 	if reOrgDb == nil {
 		reOrgDb = posdb.NewDb("forkdb")
 	}
-
 
 	numberBytes, _ := reOrgDb.Get(epochId, "reorgNumber")
 
@@ -341,24 +326,20 @@ func (f *ForkMemBlockChain) updateFork(epochId uint64) {
 	reOrgDb.Put(epochId, "forkNumber", b)
 }
 
+// func (f *ForkMemBlockChain) GetEpochGenesis(epochid uint64,lastBlk *types.Block) ([]byte,error){
 
-func (f *ForkMemBlockChain) GetEpochGenesis(epochid uint64,lastBlk *types.Block) ([]byte,error){
+// 	epGen := &EpochGenesis{}
+// 	epGen.ProtocolMagic = []byte("wanchainpos")
+// 	epGen.EpochId = epochid
+// 	epGen.PreEpochLastBlkHash = lastBlk.Hash()
+// 	epGen.RBLeaders = epochLeader.GetEpocher().GetEpochLeaders(epochid)
+// 	epGen.SlotLeaders = make([][]byte,0)
+// 	pks,err  := slotleader.GetSlotLeaderSelection().GetSlotLeaders(epochid)
+// 	if err!=nil {
+// 		for _, slpk := range pks {
+// 			epGen.SlotLeaders = append(epGen.SlotLeaders, crypto.FromECDSAPub(slpk))
+// 		}
+// 	}
 
-	epGen := &EpochGenesis{}
-	epGen.ProtocolMagic = []byte("wanchainpos")
-	epGen.EpochId = epochid
-	epGen.PreEpochLastBlkHash = lastBlk.Hash()
-	epGen.RBLeaders = epochLeader.GetEpocher().GetEpochLeaders(epochid)
-	epGen.SlotLeaders = make([][]byte,0)
-	pks,err  := slotleader.GetSlotLeaderSelection().GetSlotLeaders(epochid)
-	if err!=nil {
-		for _, slpk := range pks {
-			epGen.SlotLeaders = append(epGen.SlotLeaders, crypto.FromECDSAPub(slpk))
-		}
-	}
-
-	return json.Marshal(epGen)
-}
-
-
-
+// 	return json.Marshal(epGen)
+// }
