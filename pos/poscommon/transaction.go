e1754aa05527ada815869b9e0eed57438294180d
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2019-03-18 14:48:27 +0800

    move transaction to pscommon

---
+++ pos/poscommon/transaction.go
@@ -0,0 +1,38 @@
+package poscommon
+
+import (
+	"errors"
+	"context"
+	"github.com/wanchain/go-wanchain/rpc"
+	"github.com/wanchain/go-wanchain/common"
+	"github.com/wanchain/go-wanchain/log"
+)
+
+//type SendTxArgs struct {
+//  From     common.Address  `json:"from"`
+//  To       *common.Address `json:"to"`
+//  Gas      *hexutil.Big    `json:"gas"`
+//  GasPrice *hexutil.Big    `json:"gasPrice"`
+//  Value    *hexutil.Big    `json:"value"`
+//  Data     hexutil.Bytes   `json:"data"`
+//  Nonce    *hexutil.Uint64 `json:"nonce"`
+//}
+func SendTx(rc *rpc.Client, tx map[string]interface{}) (common.Hash, error) {
+	log.Info("begin send pos tx")
+	if rc == nil {
+		log.Error("connect rpc fail, rc is nil")
+		return common.Hash{}, errors.New("rc is not ready")
+	}
+
+	ctx := context.Background()
+	var txHash common.Hash
+	err := rc.CallContext(ctx, &txHash, "eth_sendPosTransaction", tx)
+	if nil != err {
+		log.Error("send pos tx fail", "err", err)
+		return common.Hash{}, err
+	}
+
+	log.Info("send pos tx success", "txHash", txHash)
+	return txHash, nil
+}
+
