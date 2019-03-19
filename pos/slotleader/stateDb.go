ccfc8847e665138a0dfbd0a7e7d898a6ec3bb3bf
MoLin <molin@wanchain.org>
2019-03-19 10:38:13 +0800

    Update test case of slotleader.

--- pos/slotleader/stateDb.go
+++ pos/slotleader/stateDb.go
@@ -30,59 +30,10 @@ func (s *SlotLeaderSelection) getCurrentStateDb() (stateDb *state.StateDB, err e
 	return s.stateDb, nil
 }
 
-func (s *SlotLeaderSelection) getStateDb() (stateDb *state.StateDB, err error) {
-	s.updateStateDB()
-	//s.updateToLastStateDb()
-	if s.stateDb == nil {
-		return nil, errNoStateDbInstance
-	}
-	return s.stateDb, nil
-}
-
 func (s *SlotLeaderSelection) updateToLastStateDb() {
 	stateDb, err := s.blockChain.StateAt(s.blockChain.CurrentBlock().Root())
 	if err != nil {
 		log.Error("Update stateDb error in SlotLeaderSelection.updateToLastStateDb", "error", err.Error())
-	}
-	s.stateDb = stateDb
-}
-
-func (s *SlotLeaderSelection) updateStateDB() {
-	curNumber := s.blockChain.CurrentBlock().NumberU64()
-	curSlotID := uint64((s.blockChain.CurrentBlock().Difficulty().Int64() >> 8) & 0x00ffffff)
-
-	if uint64(curSlotID) < posconfig.Stage4K {
-		log.Debug("Current Slot ID is less than SlotStage1 (4k), do not use a SafeBack2k one, use last one")
-		s.updateToLastStateDb()
-		return
-	}
-
-	if curSlotID <= SafeBack2k {
-		log.Warn("Current Slot ID is less than SafeBack2k, use last one")
-		s.updateToLastStateDb()
-		return
-	}
-
-	targetSlotID := curSlotID - SafeBack2k
-
-	backIndex := uint64(1)
-	for {
-		block := s.blockChain.GetBlockByNumber(curNumber - backIndex)
-		if block == nil {
-			log.Error("Can not find a safe block in SlotLeaderSelection.updateStateDB use last one")
-			backIndex = 0
-			break
-		}
-		slotID := uint64((block.Difficulty().Int64() >> 8) & 0x00ffffff)
-		if slotID <= targetSlotID {
-			break
-		}
-		backIndex++
-	}
-
-	stateDb, err := s.blockChain.StateAt(s.blockChain.GetBlockByNumber(curNumber - backIndex).Root())
-	if err != nil {
-		log.Error("Update stateDb error in SlotLeaderSelection.updateStateDB", "error", err.Error())
 	}
 	s.stateDb = stateDb
 }
