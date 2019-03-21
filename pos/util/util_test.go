947ee1bf08de140ed17fea02076f548a9a976a90
MoLin <molin@wanchain.org>
2019-03-21 16:26:51 +0800

    update.

--- pos/util/util_test.go
+++ pos/util/util_test.go
@@ -1,11 +1,36 @@
 package util
 
 import (
+	"encoding/hex"
 	"fmt"
 	"testing"
+
+	"github.com/wanchain/go-wanchain/crypto"
+	"github.com/wanchain/go-wanchain/pos/util"
 )
 
 func TestGetEpochSlotID(t *testing.T) {
 	epochID, slotID := GetEpochSlotID()
 	fmt.Println("epochID:", epochID, " slotID:", slotID)
+}
+
+func TestPkCompress(t *testing.T) {
+	key, _ := crypto.GenerateKey()
+	pk := &key.PublicKey
+
+	buf, err := util.CompressPk(pk)
+	if err != nil {
+		t.Fail()
+	}
+
+	fmt.Println("len(pk):", len(buf))
+
+	pkUncompress, err := util.UncompressPk(buf)
+	if err != nil {
+		t.Fail()
+	}
+
+	if hex.EncodeToString(crypto.FromECDSAPub(pk)) != hex.EncodeToString(crypto.FromECDSAPub(pkUncompress)) {
+		t.Fail()
+	}
 }
