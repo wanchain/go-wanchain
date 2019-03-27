161fa38a64d1730cd060e7936aa92415b1055f04
JeremyJiaqg <185356122@qq.com>
2019-03-27 01:37:15 -0700

    added validtor file

---
+++ pos/slotleader/slot_leader_validator.go
@@ -0,0 +1,44 @@
+package slotleader
+
+import (
+	"github.com/wanchain/go-wanchain/core/types"
+	"github.com/wanchain/go-wanchain/core/state"
+	"math/big"
+	"github.com/wanchain/go-wanchain/log"
+	"errors"
+)
+
+func (s *SLS) ValidateBody(block *types.Block) error {
+
+	extraSeal   := 65
+	header := block.Header()
+	blkTd := block.Difficulty().Uint64()
+	epochID := (blkTd >> 32)
+	slotID := ((blkTd & 0xffffffff) >> 8)
+
+	if epochID == 0 {
+		return nil
+	}
+
+	proof, proofMeg, err := s.GetInfoFromHeadExtra(epochID, header.Extra[:len(header.Extra)-extraSeal])
+
+	if err != nil {
+		log.Error("Can not GetInfoFromHeadExtra, verify failed", "error", err.Error())
+		return errors.New("Can not GetInfoFromHeadExtra, verify failed")
+
+	}
+
+	if !s.VerifySlotProof(epochID, slotID, proof, proofMeg) {
+		log.Error("VerifyPackedSlotProof failed", "number", block.NumberU64(), "epochID", epochID, "slotID", slotID)
+		return errors.New("VerifyPackedSlotProof failed")
+	}
+
+	return nil
+}
+
+func (s *SLS) ValidateState(block, parent *types.Block, state *state.StateDB, receipts types.Receipts, usedGas *big.Int) error {
+	return nil
+}
+
+
+
