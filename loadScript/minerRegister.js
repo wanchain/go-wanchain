77b0b30842fadc3dfb0a684e2e92308cb5636ace
MoLin <molin@wanchain.org>
2019-03-28 19:58:58 +0800

    update miner script.

--- loadScript/minerRegister.js
+++ loadScript/minerRegister.js
@@ -4,19 +4,22 @@
 //-------INPUT PARAMS YOU SHOULD MODIFY TO YOURS--------------------
 
 // tranValue is the value you want to stake in minValue is 100000 
-var tranValue = 100000
+var tranValue = 200000
 
-// secpub is the miner node's secpub value
+// minerAddr is the miner accounts which create nearly.
+var minerAddr = ""
+
+// secpub is the miner accounts's secpub value which is get by personal.showPublicKey
 var secpub    = ""
 
-// g1pub is the miner node's g1pub value
+// g1pub is the miner accounts's g1pub value which is get by personal.showPublicKey
 var g1pub     = ""
 
 // feeRate is the delegate dividend ratio if set to 100, means it's a single miner do not accept delegate in.
 var feeRate   = 100
 
 // lockTime do not use in POC
-var lockTime  = 10
+var lockTime  = 30
 
 // baseAddr is the fund source account.
 var baseAddr  = ""
@@ -28,12 +31,14 @@ var passwd    = ""
 
 
 //------------------RUN CODE DO NOT MODIFY------------------
+personal.unlockAccount(baseAddr, passwd)
+var pay = eth.sendTransaction({from:baseAddr, to:minerAddr, value:web3.toWin(100)})
 var cscDefinition = [{"constant":false,"inputs":[{"name":"secPk","type":"bytes"},{"name":"bn256Pk","type":"bytes"},{"name":"lockEpochs","type":"uint256"},{"name":"feeRate","type":"uint256"}],"name":"stakeIn","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"delegateAddress","type":"address"}],"name":"delegateIn","outputs":[],"payable":true,"stateMutability":"payable","type":"function"}]
 var contractDef = eth.contract(cscDefinition);
 var cscContractAddr = "0x00000000000000000000000000000000000000d2";
 var coinContract = contractDef.at(cscContractAddr);
 
 var payload = coinContract.stakeIn.getData(secpub, g1pub, lockTime, feeRate)
-var tx = eth.sendTransaction({from:eth.coinbase, to:cscContractAddr, value:web3.toWin(tranValue), data:payload, gas: 200000, gasprice:'0x' + (200000000000).toString(16)});
+var tx = eth.sendTransaction({from:baseAddr, to:cscContractAddr, value:web3.toWin(tranValue), data:payload, gas: 200000, gasprice:'0x' + (200000000000).toString(16)});
 console.log("tx=" + tx)
 //------------------RUN CODE DO NOT MODIFY------------------
