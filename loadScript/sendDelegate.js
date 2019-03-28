c342777c369e1ec67aa305276fb9e7cbcfdbaa5f
MoLin <molin@wanchain.org>
2019-03-28 20:18:25 +0800

    add a send delegate script.

---
+++ loadScript/sendDelegate.js
@@ -0,0 +1,31 @@
+// If you want to send to a delegate you can modify and use this script to run.
+
+
+//-------INPUT PARAMS YOU SHOULD MODIFY TO YOURS--------------------
+
+// tranValue is the value you want to stake in minValue is 100000 
+var tranValue = 100000
+
+// rewardAddr is the reward receive accounts which create nearly.
+var delegateAddr = ""
+
+// baseAddr is the fund source account.
+var baseAddr  = ""
+
+// passwd is the fund source account password.
+var passwd    = ""
+
+//-------INPUT PARAMS YOU SHOULD MODIFY TO YOURS--------------------
+
+
+//------------------RUN CODE DO NOT MODIFY------------------
+personal.unlockAccount(baseAddr, passwd)
+var cscDefinition = [{"constant":false,"inputs":[{"name":"secPk","type":"bytes"},{"name":"bn256Pk","type":"bytes"},{"name":"lockEpochs","type":"uint256"},{"name":"feeRate","type":"uint256"}],"name":"stakeIn","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"delegateAddress","type":"address"}],"name":"delegateIn","outputs":[],"payable":true,"stateMutability":"payable","type":"function"}]
+var contractDef = eth.contract(cscDefinition);
+var cscContractAddr = "0x00000000000000000000000000000000000000d2";
+var coinContract = contractDef.at(cscContractAddr);
+
+var payloadDelegate = coinContract.delegateIn.getData(delegateAddr)
+var tx2 = eth.sendTransaction({from:baseAddr, to:cscContractAddr, value:web3.toWin(tranValue), data:payloadDelegate, gas: 200000, gasprice:'0x' + (200000000000).toString(16)});
+console.log("tx2=" + tx2)
+//------------------RUN CODE DO NOT MODIFY------------------
