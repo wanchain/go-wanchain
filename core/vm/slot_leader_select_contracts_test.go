9d8f2eb7c33056be18d611bc842b208835770dc8
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2019-04-26 16:03:05 +0800

    update test code of slot leader contract

--- core/vm/slot_leader_select_contracts_test.go
+++ core/vm/slot_leader_select_contracts_test.go
@@ -4,7 +4,13 @@ import (
 	"crypto/ecdsa"
 	"encoding/hex"
 	"fmt"
+	"github.com/wanchain/go-wanchain/params"
+	"github.com/wanchain/go-wanchain/pos/posconfig"
+	"github.com/wanchain/go-wanchain/pos/posdb"
 	"math/big"
+	"os"
+	"path"
+	"path/filepath"
 	"testing"
 	"time"
 
@@ -12,48 +18,6 @@ import (
 	"github.com/wanchain/go-wanchain/pos/util"
 	"github.com/wanchain/go-wanchain/pos/util/convert"
 )
-
-func TestGetSlotLeaderStageIndexesKeyHash(t *testing.T) {
-
-	for i := 0; i < 4; i++ {
-		epochIDBuf := convert.Uint64ToBytes(uint64(i))
-		key := getSlotLeaderStageIndexesKeyHash(epochIDBuf, SlotLeaderStag1Indexes)
-		fmt.Printf("slot leader stage indexes stage1,epoch %v\n", epochIDBuf)
-		fmt.Println(hex.EncodeToString(key.Bytes()))
-
-		fmt.Printf("slot leader stage indexes stage2,epoch %v\n", epochIDBuf)
-		key = getSlotLeaderStageIndexesKeyHash(epochIDBuf, SlotLeaderStag2Indexes)
-		fmt.Println(hex.EncodeToString(key.Bytes()))
-	}
-}
-
-//func TestWanSlotLeaderCommitment(t *testing.T) {
-//	contract := &slotLeaderSC{}
-//
-//	contract.RequiredGas(nil)
-//
-//	contract.Run(nil, nil, nil)
-//
-//	privKey, err := crypto.GenerateKey()
-//	if err != nil {
-//		t.Fail()
-//	}
-//
-//	slot := slotleader.GetSlotLeaderSelection()
-//	epochID := uint64(4096)
-//
-//	payload, err := slot.GenerateCommitment(&privKey.PublicKey, epochID, 0)
-//	if err != nil {
-//		t.Fail()
-//	}
-//
-//	data, err := slot.PackStage1Data(payload)
-//	if err != nil {
-//		t.Fail()
-//	}
-//
-//	contract.Run(data, nil, nil)
-//}
 
 func TestStage1RlpCompress(t *testing.T) {
 	epochID := uint64(88665544)
@@ -92,7 +56,6 @@ func TestStage1RlpCompress(t *testing.T) {
 		t.Fail()
 	}
 }
-
 func TestStage2RlpCompress(t *testing.T) {
 	epochID := uint64(88665544)
 	selfIndex := uint64(789012)
@@ -167,4 +130,96 @@ func TestStage2RlpCompress(t *testing.T) {
 	if convert.Uint64ToString(selfIndex) != convert.Uint64ToString(convert.BytesToUint64(selfIndexBuf)) {
 		t.Fail()
 	}
+}
+
+func TestGetSlotLeaderStage2KeyHash(t *testing.T) {
+	for i := 0; i < 4; i++ {
+		epochIDBuf := convert.Uint64ToBytes(uint64(i))
+		for j := 0; j < 2; j++ {
+			slfIndexBuf := convert.Uint64ToBytes(uint64(j))
+			key := GetSlotLeaderStage2KeyHash(epochIDBuf, slfIndexBuf)
+			fmt.Printf("TestGetSlotLeaderStage2KeyHash epochID:%v, index:%v, key: %v\n",
+				i, j, hex.EncodeToString(key[:]))
+		}
+	}
+}
+func TestGetSlotLeaderStageIndexesKeyHash(t *testing.T) {
+
+	for i := 0; i < 4; i++ {
+		epochIDBuf := convert.Uint64ToBytes(uint64(i))
+		key := getSlotLeaderStageIndexesKeyHash(epochIDBuf, SlotLeaderStag1Indexes)
+		fmt.Printf("slot leader stage indexes stage1,epoch %v\n", epochIDBuf)
+		fmt.Println(hex.EncodeToString(key.Bytes()))
+
+		fmt.Printf("slot leader stage indexes stage2,epoch %v\n", epochIDBuf)
+		key = getSlotLeaderStageIndexesKeyHash(epochIDBuf, SlotLeaderStag2Indexes)
+		fmt.Println(hex.EncodeToString(key.Bytes()))
+	}
+}
+
+func TestIsInValidStage(t *testing.T) {
+	evm := NewEVM(Context{}, nil, &params.ChainConfig{ChainId: big1}, Config{Debug: true})
+
+	nowTime := uint64(time.Now().Unix())
+
+	baseTime := posconfig.EpochBaseTime
+	posconfig.EpochBaseTime = nowTime
+
+	testTime := nowTime + 1*posconfig.SlotCount*posconfig.SlotTime + 2*posconfig.K*posconfig.SlotTime
+	evm.Time = big.NewInt(0).SetUint64(testTime)
+
+	kStart := uint64(2 * posconfig.K)
+	kEnd := uint64(3 * posconfig.K)
+
+	if isInValidStage(0, evm, kStart, kEnd) {
+		t.Error("should out of range")
+		t.Fail()
+	}
+
+	if !isInValidStage(1, evm, kStart, kEnd) {
+		t.Error("should in range")
+		t.Fail()
+	}
+
+	testTime = nowTime + 1*posconfig.SlotCount*posconfig.SlotTime + 2*posconfig.K*posconfig.SlotTime - 1
+	evm.Time = big.NewInt(0).SetUint64(testTime)
+	if isInValidStage(1, evm, kStart, kEnd) {
+		t.Error("should in range")
+		t.Fail()
+	}
+
+	testTime = nowTime + 1*posconfig.SlotCount*posconfig.SlotTime + 3*posconfig.K*posconfig.SlotTime
+	evm.Time = big.NewInt(0).SetUint64(testTime)
+	if !isInValidStage(1, evm, kStart, kEnd) {
+		t.Error("should in range")
+		t.Fail()
+	}
+
+	testTime = nowTime + 1*posconfig.SlotCount*posconfig.SlotTime + (3*posconfig.K+1)*posconfig.SlotTime
+	evm.Time = big.NewInt(0).SetUint64(testTime)
+	if isInValidStage(1, evm, kStart, kEnd) {
+		t.Error("should out of range")
+		t.Fail()
+	}
+
+	posconfig.EpochBaseTime = baseTime
+	evm = nil
+}
+func TestAddSlotScCallTimes(t *testing.T) {
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	t.Log("Current dir path ", dir)
+	os.RemoveAll(path.Join(dir, "sl_contract_test"))
+	posdb.NewDb(path.Join(dir, "sl_contract_test"))
+
+	epochID := uint64(0)
+	loopCount := 10
+	for i := 0; i < loopCount; i++ {
+		addSlotScCallTimes(epochID)
+	}
+
+	if intByte, _ := posdb.GetDb().Get(epochID, scCallTimes); convert.BytesToUint64(intByte[:]) != uint64(loopCount) {
+		t.Fail()
+	}
+
+	os.RemoveAll(path.Join(dir, "sl_contract_test"))
 }
