69a9fa9c30a8271a3a0a45d3961b1bf7a3729b22
JeremyJiaqg <185356122@qq.com>
2019-05-09 02:25:13 -0700

    changed getReorg api

--- pos/posapi/api.go
+++ pos/posapi/api.go
@@ -146,7 +146,7 @@ func (a PosApi) Random(epochId uint64, blockNr int64) (*big.Int, error) {
 	return r, nil
 }
 
-func (a PosApi) GetReorg(epochid uint64) ([]uint64, error) {
+func (a PosApi) GetReorgState(epochid uint64) ([]uint64, error) {
 	reOrgDb := posdb.GetDbByName(posconfig.ReorgLocalDB)
 	if reOrgDb == nil {
 		return nil, errors.New("not find db")
