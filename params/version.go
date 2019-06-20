d3cee20a2f57783e03bc80b75563cb5b84a84e91
Zhang Lihua <zhang.lihua@wanchaintech.com>
2019-06-20 20:26:42 +0800

    update version

--- params/version.go
+++ params/version.go
@@ -25,7 +25,7 @@ const (
 	VersionMajor = 2        // Major version component of the current release
 	VersionMinor = 0        // Minor version component of the current release
 	VersionPatch = 0      // Patch version component of the current release
-	VersionMeta  = "beta" // Version metadata to append to the version string
+	VersionMeta  = "beta.1" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
