3a6991a4440f34d517a2978fc0e9f99938c484d7
Mo Lin <molin@wanchain.org>
2019-06-20 15:38:34 +0800

    Change default mode to fast sync mode.

--- eth/config.go
+++ eth/config.go
@@ -33,7 +33,7 @@ import (
 
 // DefaultConfig contains default settings for use on the Ethereum main net.
 var DefaultConfig = Config{
-	SyncMode:             downloader.FullSync,
+	SyncMode:             downloader.FastSync,
 	EthashCacheDir:       "wanhash",
 	EthashCachesInMem:    2,
 	EthashCachesOnDisk:   3,
