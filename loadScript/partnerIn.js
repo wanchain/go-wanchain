51f2bc014acb009bba34465ed46b05cb7cd4b26d
Mo Lin <molin@wanchain.org>
2019-06-21 12:26:06 +0800

    Update partnerIn script.

--- loadScript/partnerIn.js
+++ loadScript/partnerIn.js
@@ -3,24 +3,26 @@
 
 //-------INPUT PARAMS YOU SHOULD MODIFY TO YOURS--------------------
 
-// tranValue is the value you want to stake in minValue is 100000 
-var tranValue = 500000
+// tranValue is the value you want to partner in validator 
+var tranValue = 5000
 
-// minerAddr is the miner accounts.
-var minerAddr = ""
+// validatorAddr is the validator address.
+var validatorAddr = ""
 
-// baseAddr is the fund source account.
+// baseAddr is the partner fund source account.
 var baseAddr = ""
 
 // passwd is the fund source account password.
 var passwd = ""
+
+// bContinue set whether to continue in next period.
+var bContinue = true
 
 //-------INPUT PARAMS YOU SHOULD MODIFY TO YOURS--------------------
 
 
 //------------------RUN CODE DO NOT MODIFY------------------
 personal.unlockAccount(baseAddr, passwd)
-var pay = eth.sendTransaction({ from: baseAddr, to: minerAddr, value: web3.toWin(gasValue) })
 
 var cscDefinition = [{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"stakeAppend","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"},{"name":"lockEpochs","type":"uint256"}],"name":"stakeUpdate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"secPk","type":"bytes"},{"name":"bn256Pk","type":"bytes"},{"name":"lockEpochs","type":"uint256"},{"name":"feeRate","type":"uint256"}],"name":"stakeIn","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"},{"name":"renewal","type":"bool"}],"name":"partnerIn","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"delegateAddress","type":"address"}],"name":"delegateIn","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"delegateAddress","type":"address"}],"name":"delegateOut","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}];
 
@@ -28,7 +30,7 @@ var contractDef = eth.contract(cscDefinition);
 var cscContractAddr = "0x00000000000000000000000000000000000000DA";
 var coinContract = contractDef.at(cscContractAddr);
 
-var payload = coinContract.partnerIn.getData(minerAddr, true)
+var payload = coinContract.partnerIn.getData(validatorAddr, bContinue)
 var tx = eth.sendTransaction({ from: baseAddr, to: cscContractAddr, value: web3.toWin(tranValue), data: payload, gas: 200000, gasprice: '0x' + (200000000000).toString(16) });
 console.log("tx=" + tx)
 
