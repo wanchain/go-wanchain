2cbdbcadf206557fb034953547758f16aa55c31e
Mo Lin <molin@wanchain.org>
2019-07-23 11:08:37 +0800

    update script.

--- loadScript/updateValidator.sh
+++ loadScript/updateValidator.sh
@@ -26,23 +26,45 @@ NETWORKPATH=testnet
 
 DOCKERID=$(docker ps|grep gwan|awk '{print $1}')
 
+sudo docker pull ${DOCKERIMG}
+if [ $? -ne 0 ]; then
+    echo "Docker Pull failed. Please verify your Access of docker command."
+    echo "You can add yourself into docker group by this command, and re-login:"
+    echo "sudo usermod -aG docker ${USER}"
+    exit 1
+else
+    echo "docker pull succeed"
+fi
+
 sudo docker stop ${DOCKERID}
 
-sudo docker pull ${DOCKERIMG}
+sudo docker rm ${DOCKERID} >/dev/null 2>&1
 
-sudo docker rm ${DOCKERID}
-
-sudo docker rm gwan
+sudo docker rm gwan >/dev/null 2>&1
 
 echo ${PASSWD} | sudo tee -a /home/${USER}/.wanchain/pw.txt > /dev/null
+if [ $? -ne 0 ]; then
+    echo "Write pw.txt failed"
+    exit 1
+fi
 
 sudo docker run -d --name gwan -p 17717:17717 -p 17717:17717/udp -v /home/${USER}/.wanchain:/root/.wanchain ${DOCKERIMG} /bin/gwan ${NETWORK} --etherbase ${addrNew} --unlock ${addrNew} --password /root/.wanchain/pw.txt --mine --minerthreads=1 --wanstats ${YOUR_NODE_NAME}:admin@54.193.4.239:80
+
+if [ $? -ne 0 ]; then
+    echo "docker run failed"
+    exit 1
+fi
 
 echo 'Please wait a few seconds...'
 
 sleep 5
 
 sudo rm /home/${USER}/.wanchain/pw.txt
+
+if [ $? -ne 0 ]; then
+    echo "rm pw.txt failed"
+    exit 1
+fi
 
 echo ''
 echo ''
