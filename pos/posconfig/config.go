b9ad3211ae181ec69731460ff83f4af0c0455a7d
Mo Lin <molin@wanchain.org>
2019-08-23 15:10:04 +0800

    slot 25

--- pos/posconfig/config.go
+++ pos/posconfig/config.go
@@ -52,7 +52,7 @@ const (
 )
 const (
 	// SlotTime is the time span of a slot in second, So it's 1 hours for a epoch
-	SlotTime = 20
+	SlotTime = 25
 
 	//Incentive should perform delay some epochs.
 	IncentiveDelayEpochs = 1
