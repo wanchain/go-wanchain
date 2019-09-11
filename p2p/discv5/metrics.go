d47078350d521eafccb1cdb5439963efd83c7f6d
JeremyJiaqg <185356122@qq.com>
2019-09-11 12:20:24 +0800

    removed unused files

--- p2p/discv5/metrics.go
+++
@@ -1,8 +0,0 @@
-package discv5
-
-import "github.com/wanchain/go-wanchain/metrics"
-
-var (
-	ingressTrafficMeter = metrics.NewMeter("discv5/InboundTraffic")
-	egressTrafficMeter  = metrics.NewMeter("discv5/OutboundTraffic")
-)
