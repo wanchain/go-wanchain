e9ad8e53144ee3683f41227f1af9cd6c62abf3cc
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2019-09-17 19:40:58 +0800

    update

--- storeman/storemanmpc/step/mpc_base_step.go
+++ storeman/storemanmpc/step/mpc_base_step.go
@@ -2,48 +2,114 @@ package step
 
 import (
 	"github.com/wanchain/go-wanchain/log"
+	"github.com/wanchain/go-wanchain/p2p/discover"
 	mpcprotocol "github.com/wanchain/go-wanchain/storeman/storemanmpc/protocol"
+	"time"
 )
 
-type MpcMessageGenerator interface {
-	initialize(*[]mpcprotocol.PeerInfo, mpcprotocol.MpcResultInterface) error
-	calculateResult() error
+type BaseStep struct {
+	peers   *[]mpcprotocol.PeerInfo
+	msgChan chan *mpcprotocol.StepMessage
+	finish  chan error
+	waiting int
 }
 
-type BaseMpcStep struct {
-	BaseStep
-	messages []MpcMessageGenerator
+func CreateBaseStep(peers *[]mpcprotocol.PeerInfo, wait int) *BaseStep {
+	step := &BaseStep{peers: peers, msgChan: make(chan *mpcprotocol.StepMessage, len(*peers)+3), finish: make(chan error, 3)}
+	if wait >= 0 {
+		step.waiting = wait
+	} else {
+		step.waiting = len(*peers)
+	}
+
+	return step
 }
 
-func CreateBaseMpcStep(peers *[]mpcprotocol.PeerInfo, messageNum int) *BaseMpcStep {
-	return &BaseMpcStep{*CreateBaseStep(peers, -1), make([]MpcMessageGenerator, messageNum)}
-}
+func (step *BaseStep) InitMessageLoop(msger mpcprotocol.GetMessageInterface) error {
+	log.SyslogInfo("BaseStep.InitMessageLoop begin")
+	if step.waiting <= 0 {
+		step.finish <- nil
+	} else {
+		go func() {
+			log.SyslogInfo("InitMessageLoop begin")
 
-func (mpcStep *BaseMpcStep) InitStep(result mpcprotocol.MpcResultInterface) error {
-	for _, message := range mpcStep.messages {
-		err := message.initialize(mpcStep.peers, result)
-		if err != nil {
-			log.SyslogErr("BaseMpcStep, init msg fail. err:%s", err.Error())
-			return err
-		}
+			for {
+				err := step.HandleMessage(msger)
+				if err != nil {
+					if err != mpcprotocol.ErrQuit {
+						log.SyslogErr("InitMessageLoop fail, get message err, err:%s", err.Error())
+					}
+
+					break
+				}
+			}
+		}()
 	}
 
 	return nil
 }
 
-func (mpcStep *BaseMpcStep) FinishStep() error {
-	err := mpcStep.BaseStep.FinishStep()
-	if err != nil {
+func (step *BaseStep) Quit(err error) {
+	step.msgChan <- nil
+	step.finish <- err
+}
+
+func (step *BaseStep) FinishStep() error {
+	select {
+	case err := <-step.finish:
+		if err != nil {
+			log.SyslogErr("BaseStep.FinishStep, get a step finish error. err:%s", err.Error())
+		}
+
+		step.msgChan <- nil
 		return err
+	case <-time.After(mpcprotocol.MPCTimeOut):
+		log.SyslogErr("BaseStep.FinishStep, wait step finish timeout")
+		step.msgChan <- nil
+		return mpcprotocol.ErrTimeOut
 	}
+}
 
-	for _, message := range mpcStep.messages {
-		err := message.calculateResult()
-		if err != nil {
-			log.SyslogErr("BaseMpcStep, calculate msg result fail. err:%s", err.Error())
-			return err
+func (step *BaseStep) GetMessageChan() chan *mpcprotocol.StepMessage {
+	return step.msgChan
+}
+
+func (step *BaseStep) HandleMessage(msger mpcprotocol.GetMessageInterface) error {
+	var msg *mpcprotocol.StepMessage
+	select {
+	case msg = <-step.msgChan:
+		if msg == nil {
+			log.SyslogInfo("BaseStep get a quit msg")
+			return mpcprotocol.ErrQuit
+		}
+
+		if step.waiting > 0 && msger.HandleMessage(msg) {
+			step.waiting--
+			if step.waiting <= 0 {
+				step.finish <- nil
+			}
 		}
 	}
 
 	return nil
+}
+
+func (step *BaseStep) getPeerIndex(peerID *discover.NodeID) int {
+	for i, item := range *step.peers {
+		if item.PeerID == *peerID {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func (step *BaseStep) getPeerSeed(peerID *discover.NodeID) uint64 {
+	for _, item := range *step.peers {
+		if item.PeerID == *peerID {
+			return item.Seed
+		}
+	}
+
+	return 0
 }
