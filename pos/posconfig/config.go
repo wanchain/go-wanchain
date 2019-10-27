5dfabf4ae4e1a0d9180d99f37c15a3d0ffa61729
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2019-10-27 22:07:03 +0800

    10M-2M-25-13-20

--- pos/posconfig/config.go
+++ pos/posconfig/config.go
@@ -59,7 +59,7 @@ const (
 )
 const (
 	// SlotTime is the time span of a slot in second, So it's 1 hours for a epoch
-	SlotTime = 25
+	SlotTime = 20
 
 	//Incentive should perform delay some epochs.
 	IncentiveDelayEpochs = 1
