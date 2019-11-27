befd9570072587806621dd31be7dc2f3d2aa13eb
CryptoSalamander <dhy03196@naver.com>
2019-11-27 14:12:11 +0900

    [MOD]Add fixed one

--- accounts/abi/bind/backends/simulated.go
+++ accounts/abi/bind/backends/simulated.go
@@ -24,6 +24,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/wanchain/go-wanchain"
 	"github.com/wanchain/go-wanchain/accounts/abi/bind"
 	"github.com/wanchain/go-wanchain/common"
 	"github.com/wanchain/go-wanchain/common/math"
