fff96e62b052bcdcca4c084df15c87995049a3fe
jiaqinggang1 <forestjqg@126.com>
2020-04-24 02:48:09 -0700

    update contract

--- pos/posavgretrate/PosAvgReturn.sol
+++ pos/posavgretrate/PosAvgReturn.sol
@@ -64,15 +64,13 @@ library SafeMath {
     }
 }
 
-contract PosAvgReturn {
+contract Enhancement {
 
     using SafeMath for uint;
 
     uint public constant DIVISOR = 10000;
 
-    event UpDownReturn(uint indexed groupStartTime,uint indexed targetTime,uint indexed result);
-
-    address constant PRECOMPILE_CONTRACT_ADDR = 0xda;
+    address constant PRECOMPILE_CONTRACT_ADDR = 0x268;
 
     function getPosAvgReturn(uint256 groupStartTime,uint256 targetTime)  public view returns(uint256) {
        bytes32 functionSelector = keccak256("getPosAvgReturn(uint256,uint256)");
@@ -86,9 +84,7 @@ contract PosAvgReturn {
         if (!success) {
             return 0;
         }
-
         return result;
-
     }
 
     function callWith32BytesReturnsUint256(
@@ -102,7 +98,7 @@ contract PosAvgReturn {
 
             mstore(freePtr, functionSelector)
             mstore(add(freePtr, 4), param1)
-            mstore(add(freePtr, 32), param2)
+            mstore(add(freePtr, 36), param2)
 
             // call ERC20 Token contract transfer function
             success := staticcall(gas, to, freePtr,68, freePtr, 32)
@@ -110,4 +106,81 @@ contract PosAvgReturn {
             result := mload(freePtr)
         }
     }
+
+
+
+    function add(uint256 x1, uint256 y1, uint256 x2, uint256 y2)  public view returns(uint256 retx, uint256 rety,bool success) {
+       bytes32 functionSelector = keccak256("add(uint256, uint256, uint256, uint256)");
+       address to = PRECOMPILE_CONTRACT_ADDR;
+
+       assembly {
+           let freePtr := mload(0x40)
+            mstore(freePtr, functionSelector)
+            mstore(add(freePtr, 4), x1)
+            mstore(add(freePtr, 36), y1)
+            mstore(add(freePtr, 68), x2)
+            mstore(add(freePtr, 100), y2)
+            // call ERC20 Token contract transfer function
+            success := staticcall(gas,to, freePtr,132, freePtr, 64)
+
+            retx := mload(freePtr)
+            rety := mload(add(freePtr,32))
+        }
+
+    }
+
+    function mulG(uint256 scalar)   public view returns(uint256 x, uint256 y,bool success) {
+        bytes32 functionSelector = keccak256("mulG(uint256)");
+        address to = PRECOMPILE_CONTRACT_ADDR;
+        assembly {
+            let freePtr := mload(0x40)
+
+            mstore(freePtr, functionSelector)
+            mstore(add(freePtr, 4), scalar)
+
+            // call ERC20 Token contract transfer function
+            success := staticcall(gas, to, freePtr,36, freePtr, 64)
+
+            x := mload(freePtr)
+            y := mload(add(freePtr,32))
+        }
+
+    }
+
+    function calPolyCommit(bytes polyCommit, uint256 x)   public view returns(uint256 sx, uint256 sy,bool success) {
+       bytes32 functionSelector = keccak256("calPolyCommit(bytes,uint256)");
+       address to = PRECOMPILE_CONTRACT_ADDR;
+       uint len = polyCommit.length;
+       uint idx = polyCommit.length + 4;
+       assembly {
+           let freePtr := mload(0x40)
+            mstore(freePtr, functionSelector)
+            calldatacopy(add(freePtr, 4), polyCommit, len)
+            mstore(add(freePtr, idx), x)
+
+            // call ERC20 Token contract transfer function
+            success := staticcall(gas,to, freePtr,100, freePtr, 64)
+            sx := mload(freePtr)
+            sy := mload(add(freePtr, 32))
+        }
+    }
+
+    function enc(uint256 r, uint256 M, uint256 K)   public view returns (bytes c,bool success) {
+       bytes32 functionSelector = keccak256("enc(uint256,uint256,uint256)");
+       address to = PRECOMPILE_CONTRACT_ADDR;
+
+       assembly {
+           let freePtr := mload(0x40)
+            mstore(freePtr, functionSelector)
+            mstore(add(freePtr, 4), r)
+            mstore(add(freePtr, 36), M)
+            mstore(add(freePtr, 68), K)
+
+            // call ERC20 Token contract transfer function
+            success := staticcall(gas,to, freePtr,100, freePtr, 64)
+            c := freePtr
+            returndatacopy(c, 0, returndatasize)
+        }
+    }
+
 }
