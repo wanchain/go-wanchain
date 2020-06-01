f8097c890abe86d37df8e9879a8e270c3b8a273f
Mo Lin <molin@wanchain.org>
2020-06-01 15:46:24 +0800

    Update deployMainnetValidator.sh

--- loadScript/deployMainnetValidator.sh
+++ loadScript/deployMainnetValidator.sh
@@ -15,7 +15,7 @@ read -s PASSWD
 echo 'Confirm your password of validator account:'
 read -s PASSWD2
 echo ''
-DOCKERIMG=wanchain/client-go:2.1.4
+DOCKERIMG=wanchain/client-go:2.1.5
 
 if [ ${PASSWD} != ${PASSWD2} ]
 then
