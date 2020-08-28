08d52b04f0caad843d24c746c2aff7d25ec481b9
Mo Lin <molin@wanchain.org>
2020-08-28 18:38:53 +0800

    update testnet Mars fork point.

--- pos/posconfig/config.go
+++ pos/posconfig/config.go
@@ -108,7 +108,7 @@ const (
 	TestnetVenusEpochId = 18369
 
 	MainnetMarsEpochId = MainnetVenusEpochId
-	TestnetMarsEpochId = 18501
+	TestnetMarsEpochId = 18506
 
 	TARGETS_LOCKED_EPOCH = 90 //90 DAYS,90 EPOCH
 	RETURN_DIVIDE        = 10000
