882275d2873aea520880a474c9275d08679b331f
Mo Lin <molin@wanchain.org>
2021-06-28 14:42:02 +0800

    update docker for fd-limit

--- bootnode/dockerfile/syncbuild.sh
+++ bootnode/dockerfile/syncbuild.sh
@@ -13,5 +13,5 @@
 #cp ./build/bin/gwan ../pos6/bin/
 #cd ../pos6
 
-docker build . -t wanchain/client-go:2.2.1-beta.1
-docker push wanchain/client-go:2.2.1-beta.1
+docker build . -t wanchain/client-go:2.2.1-beta.2
+docker push wanchain/client-go:2.2.1-beta.2
