a4151404de3553fa313a75f5f96e09f12355ac93
Mo Lin <molin@wanchain.org>
2022-03-03 18:33:44 +0800

    update version

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 2        // Major version component of the current release
 	VersionMinor = 2        // Minor version component of the current release
 	VersionPatch = 1        // Patch version component of the current release
-	VersionMeta  = "beta.4" // Version metadata to append to the version string
+	VersionMeta  = "beta.5" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
