cbcd996d90943ecd22e196caabf91e3cce021f17
ubuntu20 <2627302231@qq.com>
2022-05-26 01:09:07 -0700

    change miner GasCeil from 10000000 to 50000000

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -83,7 +83,7 @@ var Defaults = Config{
 	TrieTimeout:             60 * time.Minute,
 	SnapshotCache:           102,
 	Miner: miner.Config{
-		GasCeil:  10000000,
+		GasCeil:  50000000,
 		GasPrice: big.NewInt(params.GWei),
 		Recommit: 3 * time.Second,
 	},
