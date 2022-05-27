23ac2d103b581bd68f3d1cbcea8f1963a416645a
ubuntu20 <2627302231@qq.com>
2022-05-26 20:32:51 -0700

    change parameters for performance

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -177,10 +177,10 @@ var DefaultTxPoolConfig = TxPoolConfig{
 	PriceLimit: 1,
 	PriceBump:  10,
 
-	AccountSlots: 16,
-	GlobalSlots:  4096 + 1024, // urgent + floating queue capacity with 4:1 ratio
-	AccountQueue: 64,
-	GlobalQueue:  1024,
+	AccountSlots: 16*128,
+	GlobalSlots:  40960 + 10240, // urgent + floating queue capacity with 4:1 ratio
+	AccountQueue: 64*128,
+	GlobalQueue:  10240,
 
 	Lifetime: 3 * time.Hour,
 }
